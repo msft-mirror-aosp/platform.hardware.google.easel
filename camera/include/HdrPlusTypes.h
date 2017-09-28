db154055d224885c1c722df9568dac8f6065f05d
Joshua Litt <joshualitt@google.com>
2017-09-28 14:04:59 -0700

    Add debug variable for dumping profiles from easel
    Test: cjluo's test camera app
    Bug: 65761826
    Change-Id: I01e485a8fa25f7b918641ea0e7ae69976e20219b

--- camera/include/HdrPlusTypes.h
+++ camera/include/HdrPlusTypes.h
@@ -232,6 +232,7 @@ static const uint32_t DEBUG_PARAM_NONE                      = 0u;
 static const uint32_t DEBUG_PARAM_SAVE_GCAME_INPUT_METERING = (1u);
 static const uint32_t DEBUG_PARAM_SAVE_GCAME_INPUT_PAYLOAD  = (1u << 1);
 static const uint32_t DEBUG_PARAM_SAVE_GCAME_TEXT           = (1u << 2);
+static const uint32_t DEBUG_PARAM_SAVE_PROFILE              = (1u << 3);
 
 /*
  * StaticMetadata defines a camera device's characteristics.
